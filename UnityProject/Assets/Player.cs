eb9912c591b057c4a64e79620c9268257770108b
hyp1530 <55218857+hyp1530@users.noreply.github.com>
2019-11-07 11:40:29 +0800

    1107

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("速度"), Range(1, 100)]
+    public int speed = 50;
+    [Header("跳躍"), Range(1, 100)]
+    public float jump = 2.5f;
+    public bool isGround;
+    private Rigidbody r3d;
+    // Use this for initialization
+    private void Start()
+    {
+        r3d = this.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        Walk(); 
+        Jump();
+    }
+ private void OnCollisionEnter (Collision collision)
+    {
+        isGround = true;
+        Debug.Log("碰到東西：" + collision.gameObject);
+    }
+    /// <summary>
+    /// 走路
+    /// </summary>
+    void Walk()
+    {
+        r3d.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0,0));
+        r3d.AddForce(new Vector3(0,0,speed * Input.GetAxis("Vertical")));
+
+    }
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r3d.AddForce(new Vector3(0, jump,0));
+        }
+    }
+   
+}
---
+++ UnityProject/Assets/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour {
+    [Header("對話")]
+    public string SayStart = "嗨!你好";
+    [Header("對話速度")]
+    public float speed = 1.5f;
+    [Header("任務相關")]
+    public bool Complete = false;
+    public int BateryFinish  = 5;
+    public int Batery = 0;
+    // Use this for initialization
+    void Start () {
+        
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
